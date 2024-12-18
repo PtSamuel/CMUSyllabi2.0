6fb03341dce49135ff7b83921f93dc1aaf3181fa
EC2 Default User <ec2-user@ip-172-31-87-201.ec2.internal>
2024-12-18 02:43:24 +0000

    Try-catch.

--- Runner.py
+++ Runner.py
@@ -3,27 +3,29 @@ from Crawler.Data import SyllabusRegistry
 from Crawler.Constants import Constants
 
 def main():
+    try:        
+        # Store to avoid accessing this page multiple times.
+        print('Fetching syllabus registry base url:', Constants.CMU_SYLLABUS_REGISTRY_URL.value)
+        html = get_and_unwrap(Constants.CMU_SYLLABUS_REGISTRY_URL.value)
+        sr = SyllabusRegistry(html=html)
+
+        for s in sr.semesters:
+            for d in s.departments:
+                d.get()
+                print(f'Fetching courses under {s}, {d}.')
         
-    # Store to avoid accessing this page multiple times.
-    print('Fetching syllabus registry base url:', Constants.CMU_SYLLABUS_REGISTRY_URL.value)
-    html = get_and_unwrap(Constants.CMU_SYLLABUS_REGISTRY_URL.value)
-    sr = SyllabusRegistry(html=html)
-
-    for s in sr.semesters:
-        for d in s.departments:
-            d.get()
-            print(f'Fetching courses under {s}, {d}.')
+        for s in sr.semesters:
+            for d in s.departments:
+                for c in d.courses['Available Syllabi']:
+                    print(f'Fetching course {c} under {s}, {d}')
+                    c.get()
+                    archive = c.analyze(c.result)
+                    if archive == None:
+                        print('abnormal:', c.href)
+                    else:
+                        print(archive)
+    except:
+        breakpoint()
     
-    for s in sr.semesters:
-        for d in s.departments:
-            for c in d.courses['Available Syllabi']:
-                print(f'Fetching course {c} under {s}, {d}')
-                c.get()
-                archive = c.analyze(c.result)
-                if archive == None:
-                    print('abnormal:', c.href)
-                else:
-                    print(archive)
-
 if __name__ == '__main__':
     main()
