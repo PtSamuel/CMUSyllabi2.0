7b2453a5ed07a6dccc927147d1b26f4f4fc29851
PtSamuel <46211333+PtSamuel@users.noreply.github.com>
2024-12-19 13:25:16 +0800

    Ready for the real run.

--- Runner.py
+++ Runner.py
@@ -1,37 +1,44 @@
+import pickle
+import argparse
+
 from Crawler.Utils import get_and_unwrap
 from Crawler.Data import SyllabusRegistry
 from Crawler.Constants import Constants
 from Crawler.Parallel import Parallel
 
-import pickle
-
-def main():
+def main(args):
     try:        
         # Store to avoid accessing this page multiple times.
         print('Fetching syllabus registry base url:', Constants.CMU_SYLLABUS_REGISTRY_URL.value)
         html = get_and_unwrap(Constants.CMU_SYLLABUS_REGISTRY_URL.value)
-        sr = SyllabusRegistry(html=html, ignore_archived=True)
+        
+        if args.debug:
+            sr = SyllabusRegistry(html=html, ignore_archived=True)
+            semesters = sr.semesters[:1]
+        else:
+            sr = SyllabusRegistry(html=html, ignore_archived=False)
+            semesters = sr.semesters
         
         manager = Parallel()
 
-        for s in sr.semesters:
+        for s in semesters:
             for d in s.departments:
                 print(f'Creating task for courses under {s}, {d}.')
                 def action(dep):
                     dep.get()
                 manager.add(action, d)
-            break
         manager.wait()
-        breakpoint()
         
-        for s in sr.semesters:
+        if args.debug:
+            breakpoint()
+        
+        for s in semesters:
             for d in s.departments:
                 for c in d.courses['Available Syllabi']:
                     print(f'Fetching course {c} under {s}, {d}')
                     def action(course):
                         course.get()
                     manager.add(action, c)
-            break
         manager.wait()
         
     except Exception as e:
@@ -46,4 +53,7 @@ def main():
         breakpoint()
         
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(prog='Runner')
+    parser.add_argument('-d', '--debug', action='store_true')
+    args = parser.parse_args()
+    main(args)
