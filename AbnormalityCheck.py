45670f5e6ed37e5a4659d6c476a4de8e1e7d8c0d
EC2 Default User <ec2-user@ip-172-31-95-234.ec2.internal>
2024-12-20 09:38:07 +0000

    Abnormality checker.

--- AbnormalityCheck.py
+++ AbnormalityCheck.py
@@ -3,6 +3,15 @@ sr = pickle.load(open('syllabus_registry.pkl', 'rb'))
 for s in sr.semesters:
     for d in s.departments:
         if d.processed:
+            if d.course_count == 0:
+                pass
+                # print(f'Zero course found in department {d} @ {d.href} under {s}.')
             for c in d.courses['Available Syllabi'] + d.courses['Individualized Experiences']:
-                if c.processed and c.archive is None:
-                    print(c)
+                if c.processed: 
+                    if c.archive is None:
+                        print(f'Abnormal archive: course {c} @ {c.href} under {s}, {d}.')
+                else:
+                    print(f'Unprocessed course {c} @ {c.href} under {s}, {d}.')
+        else:
+            print(f'Unprocessed department {d} @ {d.href} under {s}.')
+
