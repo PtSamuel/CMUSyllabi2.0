10a98641691c299095e524a9424601f4882f877e
EC2 Default User <ec2-user@ip-172-31-95-234.ec2.internal>
2024-12-20 14:10:55 +0000

    Good enough, but some weird pages are unnoticed.

--- AbnormalityCheck.py
+++ AbnormalityCheck.py
@@ -1,17 +1,18 @@
 import pickle
+from Crawler.Data import Status
+
 sr = pickle.load(open('syllabus_registry.pkl', 'rb'))
 for s in sr.semesters:
     for d in s.departments:
-        if d.processed:
+        if d.status == Status.SUCCESS:
             if d.course_count == 0:
                 pass
                 # print(f'Zero course found in department {d} @ {d.href} under {s}.')
             for c in d.courses['Available Syllabi'] + d.courses['Individualized Experiences']:
-                if c.processed: 
-                    if c.archive is None:
-                        print(f'Abnormal archive: course {c} @ {c.href} under {s}, {d}.')
-                else:
-                    print(f'Unprocessed course {c} @ {c.href} under {s}, {d}.')
+                if c.status != Status.SUCCESS: 
+                    print(f'Abnormal status {c.status} of course {c} @ {c.href} under {s}, {d}.')
         else:
-            print(f'Unprocessed department {d} @ {d.href} under {s}.')
+            print(f'Unvisited or failed department {d} @ {d.href} under {s}.')
 
+
+breakpoint()
