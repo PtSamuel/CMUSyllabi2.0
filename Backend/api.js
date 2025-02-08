0fadff5a71d2e0f88f74e74d80e12007e62df420
EC2 Default User <ec2-user@ip-172-31-70-49.ec2.internal>
2025-02-08 05:37:45 +0000

    API: find_course.

--- Backend/api.js
+++ Backend/api.js
@@ -5,5 +5,12 @@ api_router.get('/test', (req, res) => {
 	res.json({ course: 15213 });
 });
 
+api_router.get('/find_course', (req, res) => {
+	const params = Object.keys(req.query);
+	console.log(`Parameters: ${params}`);
+	const { course_number } = req.query;
+	res.json({ course_number: course_number });
+});
+
 module.exports = api_router;
 
