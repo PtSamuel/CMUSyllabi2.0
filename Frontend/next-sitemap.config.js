cb3f13842707bf74394cc7ce5295ad64eb0a792f
Yutian Chen <markchenyutian@gmail.com>
2025-02-08 10:06:33 -0500

    Cleanup and initial setup

--- Frontend/next-sitemap.config.js
+++ Frontend/next-sitemap.config.js
@@ -3,9 +3,7 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  // !STARTERCONF Change the siteUrl
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: 'https://PtSamuel.github.io/CMUSyllabi2.0',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
--- Frontend/src/constant/config.ts
+++ Frontend/src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'CMU Syllabus Searcher',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'One stop solution for finding the syllabus of CMU courses',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://tsnext-tw.thcl.dev',
 };
